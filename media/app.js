c39aa486f29364b411a9b71e782f03d74f8cdbb7
Romain Lussier <romdj_123@hotmail.com>
2020-04-06 11:52:17 +0200

    minor changes: pending confirmation on interest from TSN

--- media/app.js
+++ media/app.js
@@ -3,6 +3,7 @@ const { writeFileSync } = require('fs');
 const rq = require('request-promise-native');
 const useWget = true;
 const intervalExampleDate = '2020-04-02';
+const { exec } = require("child_process");
 // const intervalExampleDate = '2020-02-08';
 const intervalStartDate = '2020-02-08';
 const intervalEndDate = moment().format("YYYY-MM-DD");
@@ -23,17 +24,73 @@ const urlStructure = [
     weekSchedule: [0, 1, 2, 3, 4]
   },
   {
+    name: 'Ray Ferraro',
+    prefix: 'https://iphone.tsn.ca/tsnpodcasts/tsnradio/RayFerraroFULL',
+    dateStructure: (date) => {
+      return moment(date)
+        .format('MMMMDYYYY')
+        .toLowerCase()
+    },
+    suffix: '.mp3',
+    weekSchedule: [0, 1, 2, 3, 4]
+  },
+  {
+    name: 'Andrew Zadarnowski',
+    prefix: 'https://iphone.tsn.ca/tsnpodcasts/tsnradio/Zadarnowski',
+    dateStructure: (date) => {
+      return moment(date)
+        .format('MD')
+    },
+    suffix: '.mp3',
+    weekSchedule: [6]
+  },
+  {
     name: 'Pierre McGuire',
     dateStructure: '',
     weekSchedule: [0, 1, 2, 3, 4]
   },
 ];
 
-const marcDenis = `https://iphone.tsn.ca/tsnpodcasts/dennyapr22020.mp3`;
-const rayFerraro = `https://iphone.tsn.ca/tsnpodcasts/tsnradio/RayFerraroFULLApril22020.mp3`;
-const aaronWard = `https://iphone.tsn.ca/tsnpodcasts/tsnradio/AaronWardFULLApril22020.mp3`;
-const darrenDreger = `https://iphone.tsn.ca/tsnpodcasts/dregsapr22020.mp3`;
-const gordMiller = `https://iphone.tsn.ca/MillerMar2520.mp3`;
+const arponBasu = [
+  'https://iphone.tsn.ca/basumar2420.mp3',
+];
+const marcDenis = [
+  'https://iphone.tsn.ca/tsnpodcasts/denismar252020.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/denmar272020.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/denmar302020.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/dennyapr22020.mp3',
+];
+const bobMckenzie = [
+  'https://iphone.tsn.ca/tsnpodcasts/mckmar252020.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/mckapr32020.mp3'
+];
+const marcDumont = [
+  'https://iphone.tsn.ca/tsnpodcasts/dumomar252020.mp3'
+];
+const rayFerraro = [
+  'https://iphone.tsn.ca/FerraroMar2420.mp3',
+  'https://iphone.tsn.ca/FerraroMar2620.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/tsnradio/RayFerraroFULLApril22020.mp3',
+];
+const aaronWard = [
+  'https://iphone.tsn.ca/tsnpodcasts/tsnradio/AaronWardFULLApril22020.mp3'
+];
+const darrenDreger = [
+  'https://iphone.tsn.ca/tsnpodcasts/dregsapr22020.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/dregmar262020.mp3'
+];
+const gordMiller = [
+  'https://iphone.tsn.ca/MillerMar2520.mp3'
+];
+const ericMacramalla = [
+  'https://iphone.tsn.ca/tsnpodcasts/macapr22020.mp3'
+];
+const pierreMcGuire = [
+  'https://iphone.tsn.ca/McGuireMar2320.mp3',
+  'https://iphone.tsn.ca/McGuireMar2520.mp3',
+  'https://iphone.tsn.ca/McGuireMar2620.mp3',
+  'https://iphone.tsn.ca/tsnpodcasts/tsnradio/McGuireApril32020.mp3',
+];
 
 // console.log(moment().format('MMM'));
 // console.log(urlStructure[0].dateStructure(intervalStartDate));
@@ -50,8 +107,8 @@ function isIntervieweeDay(date, interviewee) {
 
 function downloadFile(path, interviewee) {
   console.log(`fetching: ${path}`);
-  if(useWget)
-  process.exec
+  if (useWget)
+    process.exec
   rq.get(path)
     .then(data => writeFileSync(`output/${interviewee.name.replace(' ', '_').toLowerCase()}${date.format('DDMMMYYYY')}.mp3`, Buffer.from(data)));
 }
