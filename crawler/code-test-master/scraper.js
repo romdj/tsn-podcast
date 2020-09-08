ef5d5c8e49af50cf4765ab1fa14a636cd937bbc6
Romain Lussier <romdj_123@hotmail.com>
2020-09-08 23:27:52 +0200

    feature build done for parsing elements

--- crawler/code-test-master/scraper.js
+++ crawler/code-test-master/scraper.js
@@ -27,147 +27,31 @@ const parsePodcastURL = async (url) => {
   return [legislator]
 }
 
-const getLegislatorsFromMultiplePage = async () => {
-  const urls = await getRPsURLs()
-  let legislators = await Promise.all(
-    urls.map(async url => {
-      try {
-        const page = await fetchPage(url)
-        const legislator = { url: url }
-        Object.assign(legislator, parseMEPInfo(page))
-        legislator.url = url
-        return legislator
-      }
-      catch (err) {
-        console.log(`error parsing MEP: ${JSON.stringify(err)}`)
-      }
-    }))
-
-  return legislators
-}
-
-const getStructuredName = fullName => {
-  return { fullName, firstName: getFirstName(fullName), lastName: getLastName(fullName) }
-}
-
-const getFirstName = name => {
-  const firstNames = name.split(' ').map(subName => (!isUpperCase(subName) ? subName : ''))
-  return firstNames.join(' ')
-}
-
-const getLastName = (name) => {
-  const lastNames = name.split(' ').map(subName => (isUpperCase(subName) ? subName : ''))
-  return lastNames.join(' ').trim()
-}
-
-const isUpperCase = text => text[1] == text[1].toUpperCase()
-
-const getRPsURLs = async (srcUrl) => {
-  srcUrl = srcUrl ? srcUrl : 'https://www.europarl.europa.eu/meps/en/full-list/all'
-  const urls = []
-  const page = await fetchPage(srcUrl)
-  const $ = cheerio.load(page)
-  $('.erpl_member-list-item').map((index, element) => {
-    const url = $(element).children('a').attr('href')
-    if (validateUrl(url))
-      urls.push(url)
-  })
-  return urls
-}
-
-const validateUrl = (url) => {
-  return typeof url === 'string'
-}
-
-const fetchPage = async (url) => {
-  return await rq.get(url)
-}
-
-const parseMEPInfo = (page) => {
-  const fullName = getFullName(page)
-  return {
-    name: fullName.fullName,
-    lastName: fullName.lastName,
-    partyGroup: getParty(page)
-      .split('-')[0].trim()
-      .split('\n')[0].trim(),
-    country: getCountry(page),
-    image: getPhotoUrl(page)
-  }
-}
-
-const getPhotoUrl = (page) => {
-  const $ = cheerio.load(page)
-  const photoUrl = $('.erpl_image-frame').children('span').children('img').attr('src')
-  return `${websiteRoot}${photoUrl}`
-}
-
 const parsePodcastInfo = (page) => {
-  const link = parseNowPLaying(page);
+  const link = parseNowPLayingMediaURL(page);
   const title = parseNowPlayingTitle(page);
   return { link, title };
 }
 
 const parseNowPlayingTitle = (page) => {
   const $ = cheerio.load(page)
-  // console.log($('div .headline'));
-  const something = $('div .headline')
-    .map((index, element) => {
-      console.log(`index:${index}`);
-      console.log(`element:${JSON.stringify(element, function (key, value) {
-        if (key == 'parent') { return 'circular something'; }
-        else { return value; }
-      })}`);
-
-      return this
-    }).get();// .children('h1')
-  console.log(`something:${something}`);
-  throw new Error('NOT IMPLEMENTED YET');
+  return $('div .headline')[6].children[1].children[0].data;
 }
+
 const parseNowPlayingDescription = (page) => {
   const $ = cheerio.load(page)
-  return $('div .headline').children('p .lead')
+  const result = $('div .headline')[6].children[3].children[0].data.split('\n').map(s => s.trim()).join(' ');
+  return result;
 
-  throw new Error('NOT IMPLEMENTED YET');
 }
-const parseNowPLaying = (page) => {
+const parseNowPLayingMediaURL = (page) => {
   const $ = cheerio.load(page)
-
-  // div content-container
-  // div content-wrapper
-  // section content
-  // div content-main
-  // div audioContainer
-  // article standard-audio-article
-  // div article-media
-  // div mejs-container svg mejs-audio
-  // div mejs-inner
-  // div mejs-mediaelement
-  // audio .src
-
-  return $('audio').attr('src')
-  // const mediaUrl = $('audio').map(something => console.log(something));
-
-  // children('audio').attr('src')
-  // console.log(`fetching podcast link: ${mediaUrl}`);
-
-
-
-  // /html/body/div[3]/div[3]/section/div[3]/div[1]/article/div/div[1]/div/div[1]/audio
-  // *[@id="audioPlayer_1_1512459"]
-
+  return $('audio').attr('src');
 }
 
-
-// const url = `${radioWebsiteRoot}/audio`
-// const page = await fetchPage(url);
-// parseNowPLaying(page);
-const page = readFileSync('./test/data/sampleMTLPage.html');
-parseNowPlayingTitle(page);
-
-module.exports(
+module.exports = {
   parseTSNPodcasts,
   parseNowPlayingTitle,
   parseNowPlayingDescription,
-  parseNowPLaying
-)
+  parseNowPLayingMediaURL
+};
