17c014783547c3386e0240822d0a861d116072b8
Romain Lussier <romdj_123@hotmail.com>
2020-10-08 14:51:07 +0200

    transforming into single app repo

--- handler.js
+++ handler.js
@@ -1,7 +1,9 @@
+const http = require('http');
 
 exports.handler = (event, context, callback) => {
   // const url = event.url | require('./tsnRadios.json')['Montreal'];
   const url = 'https://www.tsn.ca/radio/montreal-690';
+
 
   child.stdout.on('data', console.log);
   child.stderr.on('data', console.error);
